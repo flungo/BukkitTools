09d2360d5d306706b5f8738ba2180d88a3761ae3
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-13 13:27:20 +0000

    Log is deprecated

--- src/main/java/me/flungo/bukkit/tools/Log.java
+++ src/main/java/me/flungo/bukkit/tools/Log.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Deprecated
 public class Log {
 
     public final Logger logger = Logger.getLogger("MineCraft");
