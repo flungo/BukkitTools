0929bb98511797f9cdee8910d90fa5e48b006cdb
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-29 12:15:24 +0000

    Deprecated Log again. Does nothing the default logger doesn't do.

--- src/main/java/me/flungo/bukkit/tools/Log.java
+++ src/main/java/me/flungo/bukkit/tools/Log.java
@@ -8,6 +8,7 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Deprecated
 public class Log {
 
     private final JavaPlugin plugin;
