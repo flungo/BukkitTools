8c94d170659b8e5051f9abf3c426ec5b3405615c
Fabrizio Lungo <fab@lungo.co.uk>
2013-11-30 20:16:00 +0000

    Added JavaDocs for ConfigAccessor

--- src/main/java/me/flungo/bukkit/tools/ConfigAccessor.java
+++ src/main/java/me/flungo/bukkit/tools/ConfigAccessor.java
@@ -29,6 +29,14 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * An accessor class for a YAML configuration file with similar accessors as to
+ * those provided by <code>JavaPlugin</code> for the default confg.yml
+ * configuration file.
+ *
+ * @author Edmond Poon
+ * @author Fabrizio Lungo <fab@lungo.co.uk>
+ */
 public class ConfigAccessor {
 
     private final String fileName;
@@ -36,6 +44,20 @@ public class ConfigAccessor {
     private File configFile;
     private FileConfiguration fileConfiguration;
 
+    /**
+     * Create a <code>ConfigAccessor</code> which will allow you to access the
+     * configuration for the specified fileName as you would with a default
+     * plugin config.yml through the ConfigAccessor.
+     *
+     * Creates an accessor for a configuration file stored in the
+     * <code>plugin</code>'s configuration folder at the path specified by the
+     * <code>fileName</code> where the default file for that configuration, is
+     * stored in the same path under the root of the plugin jar.
+     *
+     * @param plugin the plugin where the which the ConfigAccessor is acting for
+     * @param fileName the name of the file that the configAccessor will allow
+     * access to.
+     */
     public ConfigAccessor(JavaPlugin plugin, String fileName) {
         if (plugin == null) {
             throw new IllegalArgumentException("plugin cannot be null");
@@ -52,6 +74,10 @@ public class ConfigAccessor {
         this.configFile = new File(plugin.getDataFolder(), fileName);
     }
 
+    /**
+     * Reloads the configuration represented by this accessor from the hard
+     * disk.
+     */
     public void reloadConfig() {
         fileConfiguration = YamlConfiguration.loadConfiguration(configFile);
 
@@ -63,6 +89,12 @@ public class ConfigAccessor {
         }
     }
 
+    /**
+     * Gets the configuration file represented by this accessor.
+     *
+     * @return FileConfiguration for the configuration file represented by this
+     * accessor.
+     */
     public FileConfiguration getConfig() {
         if (fileConfiguration == null) {
             this.reloadConfig();
@@ -70,6 +102,10 @@ public class ConfigAccessor {
         return fileConfiguration;
     }
 
+    /**
+     * Saves the configuration represented by this accessor which is currently
+     * stored in memory to the hard disk.
+     */
     public void saveConfig() {
         if (fileConfiguration == null || configFile == null) {
             return;
@@ -82,6 +118,10 @@ public class ConfigAccessor {
         }
     }
 
+    /**
+     * If the configuration file does not exist on the hard disk, saves the
+     * default configuration file to the hard disk.
+     */
     public void saveDefaultConfig() {
         if (!configFile.exists()) {
             this.plugin.saveResource(fileName, false);
